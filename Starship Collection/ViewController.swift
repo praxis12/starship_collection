6db4cfe5199d50628184b3d24d745cfc3e9eaec3
Josh Arntz <jarntz12@gmail.com>
2017-04-11 10:13:21 -0500

    Initial Commit

---
+++ Starship Collection/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Starship Collection
+//
+//  Created by Josh Arntz on 4/11/17.
+//  Copyright © 2017 Mr. Bear. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
